3bfe8be17f43dd9fe32765ef0bcfa4daf1f18dfa
Garox7 <giuseppe.garozzo2@icloud.com>
2022-10-21 17:23:33 +0200

    javascript function add

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,43 @@
-const btnClick = document.getElementsById('button');
+const btnClick = document.getElementById('button');
+console.log(btnClick)
 
 btnClick.addEventListener('click',
     function () {
-        let userName = document.getElementById("user__name").value;
+        console.log('Hai cliccato! bravo!')
+
+        //NAME
+        let userName = document.getElementById("user_name").value;
         document.getElementById("name").innerHTML = userName;
+
+        //DISTANCE
+        const distance = document.getElementById('distance').value;
+
+        //AGE
+        const age =document.getElementById('age_select').value;
+
+        //TICKET PRICE SET
+        const ticketPrice = distance * 0.21;
+
+        if (age == 'minorenne') {
+            discountRate = 0.20;
+        
+        } else if (age == 'over') {
+            discountRate = 0.40;
+        
+        } else {
+            discountRate = 0;
+        }
+
+
+        let totalPrice = ticketPrice - (ticketPrice * discountRate);
+        totalPrice = parseFloat(totalPrice.toFixed(2))
+        document.getElementById('ticket_price').innerHTML =  '€' +  totalPrice;
+
+        //DEBUG
+        console.log('Distanza:',distance)
+        console.log('Età:',age)
+        console.log('Prezzo biglietto: €' + ticketPrice)
+        console.log('Prezzo bisglietto con sconto: €' + totalPrice)
+
     }
 );
